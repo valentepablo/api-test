8634b695ed0a8d82553b9b5931fec604673fafff
Pablo Valente <pablo.nvalente@gmail.com>
2023-03-01 20:30:20 -0300

    leo

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', products);
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
-app.listen(5000, () => {
-  console.log(`Listening on port 5000`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
